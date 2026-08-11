9bbba5d7537ec057ca616aaa4999b928e0a0ab47
Kavinaya0120 <kaviskk5097@gmail.com>
2026-08-11 14:14:37 +0530

    Create 2491-SmallestEvenMultiple

---
+++ 2491-SmallestEvenMultiple/2491-SmallestEvenMultiple.java
@@ -0,0 +1,11 @@
+// Last updated: 8/11/2026, 2:14:37 PM
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0)
+        return n;
+        
+        n=n*2;
+        return n;
+       
+    }
+}
