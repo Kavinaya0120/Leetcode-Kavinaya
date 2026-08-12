715a721d054bac282d2eebb7b00ce65538ea78bf
Kavinaya0120 <kaviskk5097@gmail.com>
2026-08-12 22:04:43 +0530

    Create

---
+++ problem-1786552484105/version/java/problem-1786552484105_20260812_220444.java
@@ -0,0 +1,21 @@
+// Last updated: 8/12/2026, 10:04:44 PM
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3
+4        for (int i = digits.length - 1; i >= 0; i--) {
+5
+6            if (digits[i] < 9) {
+7                digits[i]++;
+8                return digits;
+9            }
+10
+11            digits[i] = 0;
+12        }
+13
+14        int[] result = new int[digits.length + 1];
+15
+16        result[0] = 1;
+17
+18        return result;
+19    }
+20}
