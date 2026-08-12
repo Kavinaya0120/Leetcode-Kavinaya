f796168537f72e20e80457c1d7f01810c1eeff98
Kavinaya0120 <kaviskk5097@gmail.com>
2026-08-12 23:40:40 +0530

    Create 222-CountCompleteTreeNodes

---
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
@@ -0,0 +1,28 @@
+// Last updated: 8/12/2026, 11:40:40 PM
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        if (leftHeight == rightHeight) {
+            return (1 << leftHeight) + countNodes(root.right);
+        } else {
+            return (1 << rightHeight) + countNodes(root.left);
+        }
+    }
+
+    private int getHeight(TreeNode node) {
+        int height = 0;
+
+        while (node != null) {
+            height++;
+            node = node.left;
+        }
+
+        return height;
+    }
+}
